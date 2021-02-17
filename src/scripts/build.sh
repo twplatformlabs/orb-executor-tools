5146f2e721d39e331948d032c7576baefe4c39c2
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-17 17:24:34 -0600

    [ncheneweth] build.yaml and tests

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -6,7 +6,7 @@
 #
 # REGISTRY          = Registry and image name values for naming the build.
 # IMAGE
-# AWS_ECR_IMAGE     = support for aws ecr url, e.g., 123*******.dkr.ecr.us-east-1.amazonaws.com/org/repo:0.1
+# AWS_ECR_REPOSITORY     = support for aws ecr url, e.g., 123*******.dkr.ecr.us-east-1.amazonaws.com/org/repo:0.1
 #
 # TAG               = image tag, e.g., v1.0.0, latest
 
@@ -15,7 +15,7 @@ build_image() {
     pwd
     docker build "${EXTRA_BUILD_ARGS}" -t "${REGISTRY}/${IMAGE}:${TAG}" -f "${PATH}/${DOCKERFILE}" 
   else
-    docker build "${EXTRA_BUILD_ARGS}" -t "${AWS_ECR_IMAGE}:${TAG}" -f "${PATH}/${DOCKERFILE}"
+    docker build "${EXTRA_BUILD_ARGS}" -t "${AWS_ECR_REPOSITORY}/${IMAGE}:${TAG}" -f "${PATH}/${DOCKERFILE}"
   fi
 }
 
