0890f29e390f39f2116b89df01ed98511741b2e0
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-19 16:49:43 -0600

    [ncheneweth] push.yaml and tests

--- src/scripts/assume-role.sh
+++ src/scripts/assume-role.sh
@@ -4,7 +4,7 @@
 # AWS_ROLE
 # AWS_ACCESS_KEY_ID
 # AWS_SECRET_ACCESS_KEY
-# AWS_REGION
+# AWS_DEFAULT_REGION
 
 Assume () {
   TMP="$(aws sts assume-role --output json --role-arn "${AWS_ROLE}" --role-session-name "orb-exeecutor-tools-pipeline" || { echo 'sts failure!' ; exit 1; })"
@@ -13,13 +13,14 @@ Assume () {
   export SECRET_KEY=$(echo $TMP | jq -r ".Credentials.SecretAccessKey")
   export SESSION_TOKEN=$(echo $TMP | jq -r ".Credentials.SessionToken")
 
-  # write role session credentials to ephemeral executor 
+  # write role session credentials to ephemeral executor
+  mkdir ~/.aws
   cat <<EOF > ~/.aws/credentials
 [default]
 aws_access_key_id=${ACCESS_KEY}
 aws_secret_access_key=${SECRET_KEY}
 aws_session_token=${SESSION_TOKEN}
-region=${AWS_REGION}
+region=${AWS_DEFAULT_REGION}
 EOF
 }
 
