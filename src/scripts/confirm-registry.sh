64098a11d727668d8c06daad9b64b1dcc2a2e4ee
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-19 10:41:55 -0600

    [ncheneweth] push.yaml and tests

--- src/scripts/confirm-registry.sh
+++ src/scripts/confirm-registry.sh
@@ -11,35 +11,28 @@
 # AWS_SECRET_ACCESS_KEY
 # AWS_REGION
 
-confirm_registry() {
-  if [ ! "${AWS_ECR}" ]; then
-      if [ ! "${DOCKER_LOGIN}" ]; then
-        echo "registry username is not defined."
-        exit 1
-      fi
-      if [ ! "${DOCKER_PASSWORD}" ]; then
-        echo "registry password is not defined."
-        exit 1
-      fi
-      echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin "${REGISTRY}"
-  else
-      if [ ! "${AWS_ACCESS_KEY_ID}" ]; then
-        echo "ecr access key id is not defined."
-        exit 1
-      fi
-      if [ ! "${AWS_SECRET_ACCESS_KEY}" ]; then
-        echo "ecr secret access key is not defined."
-        exit 1
-      fi
-      if [ ! "${AWS_REGION}" ]; then
-        echo "aws region is not defined."
-        exit 1
-      fi
-    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REGISTRY}/${IMAGE}"
-  fi
-}
-
-# Will not run if sourced from another script. This is done so this script may be tested.
-if [[ "$_" == "$0" ]]; then
-    confirm_registry
+if [ ! "${AWS_ECR}" ]; then
+    if [ ! "${DOCKER_LOGIN}" ]; then
+      echo "registry username is not defined."
+      exit 1
+    fi
+    if [ ! "${DOCKER_PASSWORD}" ]; then
+      echo "registry password is not defined."
+      exit 1
+    fi
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin "${REGISTRY}"
+else
+    if [ ! "${AWS_ACCESS_KEY_ID}" ]; then
+      echo "ecr access key id is not defined."
+      exit 1
+    fi
+    if [ ! "${AWS_SECRET_ACCESS_KEY}" ]; then
+      echo "ecr secret access key is not defined."
+      exit 1
+    fi
+    if [ ! "${AWS_REGION}" ]; then
+      echo "aws region is not defined."
+      exit 1
+    fi
+  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REGISTRY}/${IMAGE}"
 fi
