5380b04acf1fedc6af5a63ab369e510d20000c0c
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-19 11:16:43 -0600

    [ncheneweth] push.yaml and tests

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -12,10 +12,10 @@
 # AWS_SECRET_ACCESS_KEY
 # AWS_REGION
 
-echo "IMAGE: ${REGISTRY}/${IMAGE}:${TAG}"
+echo "IMAGE: $REGISTRY/$IMAGE:$TAG"
 if [ ! "${AWS_ECR}" ]; then
     echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin "${REGISTRY}"
 else
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REGISTRY}/${IMAGE}"
 fi
-docker push "${REGISTRY}/${IMAGE}:${TAG}"
+docker push "$REGISTRY/$IMAGE:$TAG"
