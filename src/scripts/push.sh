3232b9344d793a88457400ad9df6a525db332530
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-19 10:37:17 -0600

    [ncheneweth] push.yaml and tests

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -12,19 +12,19 @@
 # AWS_SECRET_ACCESS_KEY
 # AWS_REGION
 
-push() {
-  if [ ! "${AWS_ECR}" ]; then
-      echo 'docker login'
-      echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin "${REGISTRY}"
-  else
-      echo 'aws login'
-      aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REGISTRY}/${IMAGE}"
-  fi
-  echo 'push'
-  docker push "${REGISTRY}/${IMAGE}:${TAG}"
-}
+# push() {
+if [ ! "${AWS_ECR}" ]; then
+    echo 'docker login'
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin "${REGISTRY}"
+else
+    echo 'aws login'
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${REGISTRY}/${IMAGE}"
+fi
+echo 'push'
+docker push "${REGISTRY}/${IMAGE}:${TAG}"
+# }
 
 # Will not run if sourced from another script. This is done so this script may be tested.
-if [[ "$_" == "$0" ]]; then
-    push
-fi
+# if [[ "$_" == "$0" ]]; then
+#     push
+# fi
