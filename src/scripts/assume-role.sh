6f6f3a1c2464d718a421ee78c5056060d17b18b7
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-02-19 18:09:39 -0600

    [ncheneweth] push.yaml and tests

--- src/scripts/assume-role.sh
+++ src/scripts/assume-role.sh
@@ -13,17 +13,17 @@ Assume () {
   echo "${AWS_ROLE}"
   TMP="$(aws sts assume-role --output json --role-arn ${AWS_ROLE} --role-session-name 'orb-exeecutor-tools-pipeline' || { echo 'sts failure!' ; exit 1; })"
 
-  ACCESS_KEY=$(echo $TMP | jq -r ".Credentials.AccessKeyId")
-  SECRET_KEY=$(echo $TMP | jq -r ".Credentials.SecretAccessKey")
-  SESSION_TOKEN=$(echo $TMP | jq -r ".Credentials.SessionToken")
+  export AWS_ACCESS_KEY_ID=$(echo $TMP | jq -r ".Credentials.AccessKeyId")
+  export AWS_SECRET_ACCESS_KEY=$(echo $TMP | jq -r ".Credentials.SecretAccessKey")
+  export AWS_SESSION_TOKEN=$(echo $TMP | jq -r ".Credentials.SessionToken")
 
   # write role session credentials to ephemeral executor
-  mkdir +p ~/.aws
-  cat <<EOF > ~/.aws/credentials
-[default]
-aws_access_key_id=${ACCESS_KEY}
-aws_secret_access_key=${SECRET_KEY}
-aws_session_token=${SESSION_TOKEN}
-region=${AWS_DEFAULT_REGION}
-EOF
+#   mkdir +p ~/.aws
+#   cat <<EOF > ~/.aws/credentials
+# [default]
+# aws_access_key_id=${ACCESS_KEY}
+# aws_secret_access_key=${SECRET_KEY}
+# aws_session_token=${SESSION_TOKEN}
+# region=${AWS_DEFAULT_REGION}
+# EOF
 }
