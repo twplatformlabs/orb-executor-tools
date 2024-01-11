f939b05fcf00b114e8bbfbb8bad3fb91575865b3
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-11 15:32:36 -0600

    [nc] test cleanup post integration test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/delete_github_package.sh
@@ -0,0 +1,15 @@
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+
+
+
+
+
+# curl -X POST \
+# -H "Accept: application/vnd.github.package-deletes-preview+json" \
+# -H "Authorization: bearer TOKEN" \
+# -d '{"query":"mutation { deletePackageVersion(input:{packageVersionId:\"MDIyOlJlZ2lzdHJ5UGFja2FnZVZlcnNpb243MTExNg==\"}) { success }}"}' \
+# HOSTNAME/graphql
