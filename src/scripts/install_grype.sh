d9bb74cb58f36f4348e1c2670a68597937bc3b4a
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-06 14:32:27 -0500

    [nc] test package installs
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_grype.sh
+++ src/scripts/install_grype.sh
@@ -12,7 +12,7 @@ install_grype_cli() {
     echo "Installing grype version v${GRYPE_VERSION}"
     curl -L "https://github.com/anchore/grype/releases/download/v${GRYPE_VERSION}/grype_${GRYPE_VERSION}_linux_amd64.tar.gz" --output "grype_${GRYPE_VERSION}_linux_amd64.tar.gz"
     sudo tar -xzf "grype_${GRYPE_VERSION}_linux_amd64.tar.gz" -C /usr/local/bin grype
-    rm "grype_${GRYPE_VERSION}__linux_amd64.tar.gz"
+    rm "grype_${GRYPE_VERSION}_linux_amd64.tar.gz"
 }
 
 if [[ "$GRYPE_VERSION" == "latest" ]]; then
