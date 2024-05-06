0791855d23fe9b6f697209fd31194c83edb2addd
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-06 14:26:36 -0500

    [nc] test package installs
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_bats.sh
+++ src/scripts/install_bats.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 echo "bats install depends on nodejs and npm."
 
 if [[ "$BATS_VERSION" == "latest" ]]; then
-    sudo npm install -g bats
+    npm install -g bats
 else
     npm install -g bats@"$BATS_VERSION"
 fi
--- src/scripts/install_snyk.sh
+++ src/scripts/install_snyk.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 echo "snyk install depends on nodejs and npm."
 
 if [[ "$SNYK_VERSION" == "latest" ]]; then
-    sudo npm install -g snyk
+    npm install -g snyk
 else
     npm install -g snyk@"$SNYK_VERSION"
 fi
