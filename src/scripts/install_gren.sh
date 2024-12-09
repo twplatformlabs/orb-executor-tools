8fd4de338d2d9aea8cb156157de0284605489b89
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-12-09 16:14:17 -0600

    [nc] add other executor-builder tools and update integration test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ src/scripts/install_gren.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "gren install depends on nodejs and npm."
+
+if [[ "$GREN_VERSION" == "latest" ]]; then
+    npm install -g gren
+else
+    npm install -g bats@"$GREN_VERSION"
+fi
