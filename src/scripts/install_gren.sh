71442b37ab344401a45357084dd3a3b6c8373b1d
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-12-09 16:37:27 -0600

    [nc] add other executor-builder tools and update integration test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_gren.sh
+++ src/scripts/install_gren.sh
@@ -6,5 +6,5 @@ echo "gren install depends on nodejs and npm."
 if [[ "$GREN_VERSION" == "latest" ]]; then
     npm install -g gren
 else
-    npm install -g bats@"$GREN_VERSION"
+    npm install -g gren@"$GREN_VERSION"
 fi
--- src/scripts/install_oras.sh
+++ src/scripts/install_oras.sh
@@ -12,9 +12,9 @@ install_oras_cli() {
     echo "Installing oras version v${ORAS_VERSION}"
     curl -LO "https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_linux_amd64.tar.gz"
     mkdir -p oras-install &&
-    tar -zxf "oras_${ORAS_VERSION}_*.tar.gz" -C oras-install/
+    tar -zxf "oras_${ORAS_VERSION}_linux_amd64.tar.gz" -C oras-install/
     sudo mv oras-install/oras /usr/local/bin/
-    rm -rf "oras_${ORAS_VERSION}_*.tar.gz" oras-install/
+    rm -rf "oras_${ORAS_VERSION}_linux_amd64.tar.gz" oras-install/
 }
 
 if [[ "$ORAS_VERSION" == "latest" ]]; then
