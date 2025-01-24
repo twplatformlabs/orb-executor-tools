7cb6b23174acd96f0e86c4eade4c93dc44f867f2
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-24 16:25:34 -0600

    [nc] confirm build artifacts
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/validate_file_exists.sh
+++ src/scripts/validate_file_exists.sh
@@ -2,9 +2,9 @@
 set -eo pipefail
 
 echo "validate file:${FILENAME} exists"
-if [ -f $FILENAME ]; then
+if [ -f "$FILENAME" ]; then
    echo "$FILENAME contents"
-   cat $FILENAME
+   cat "$FILENAME"
 else
    echo "File $FILENAME does not exist."
    exit 1
