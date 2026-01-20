4447024c98bbe78567f20fe59711daf33ded98c2
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-01-19 21:45:39 -0600

    [nc] iterate on integration test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/set_labels.sh
+++ test/set_labels.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 {
-  echo "export CREATE=${NOW}" 
+  echo "export CREATED=${NOW}" 
   echo "export VERSION=${CIRCLE_SHA1}"
   echo "export ALPINE_BASE=\"$(head -n 1 test/Dockerfile.alpine)\""
   echo "export UBUNTU_BASE=\"$(head -n 1 test/Dockerfile.ubuntu)\""
