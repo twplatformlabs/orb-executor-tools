dc94ba039611d030e4d628d21fcb3b327e83f79f
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-01-19 20:19:21 -0600

    [nc] shellcheck fix
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/set_labels.sh
+++ test/set_labels.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-echo "export CREATE=$NOW" >> "$BASH_ENV"
-echo "export VERSION=$CIRCLE_SHA1" >> "$BASH_ENV"
-echo "export ALPINE_BASE=$(head -n 1 test/Dockerfile.alpine)" >> "$BASH_ENV"
-echo "export UBUNTU_BASE=$(head -n 1 test/Dockerfile.ubuntu)" >> "$BASH_ENV"
+{
+  echo "export CREATE=$NOW" 
+  echo "export VERSION=$CIRCLE_SHA1"
+  echo "export ALPINE_BASE=$(head -n 1 test/Dockerfile.alpine)"
+  echo "export UBUNTU_BASE=$(head -n 1 test/Dockerfile.ubuntu)"
+} >> "$BASH_ENV"
