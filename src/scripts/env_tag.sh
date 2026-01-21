1a22b16a2cd718570345f8840d67409b636f1397
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-01-20 21:31:34 -0600

    [nc] iterate on integration test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/env_tag.sh
+++ src/scripts/env_tag.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "Add tag parameter to BASH_ENV."
+echo "Add $TAG parameter to BASH_ENV."
+echo "CIRCLE_SHA1=$CIRCLE_SHA1"
+echo "CIRCLE_TAG=$CIRCLE_TAG"
+
 if [[ -z "$CIRCLE_TAG" ]]; then
-  echo "export TAG=$CIRCLE_TAG" >> "$BASH_ENV"
-else
   echo "export TAG=$TAG" >> "$BASH_ENV"
+else
+  echo "export TAG=$CIRCLE_TAG" >> "$BASH_ENV"
 fi
